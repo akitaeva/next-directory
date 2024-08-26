8b7efa0535d5738f4a47278f6856be59c9cf03d1
Alina Kitaeva <akitaeva@gmail.com>
2024-08-26 15:19:30 -0400

    adds edit action to update snippet record in db

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -3,6 +3,7 @@
 import type { Snippet } from '@prisma/client';
 import Editor from '@monaco-editor/react';
 import { useState } from 'react';
+import { editSnippet  } from '@/actions';
 
 interface SnippetEditFormProps {
   snippet: Snippet;
@@ -15,6 +16,8 @@ export default function SnippetEditForm ({ snippet } : SnippetEditFormProps ) {
     setCode(value)
   };
 
+  const editSnippetAction = editSnippet.bind(null, snippet.id, code)
+
   return (
     <div>
       <h1>{snippet.title}</h1>
@@ -26,6 +29,11 @@ export default function SnippetEditForm ({ snippet } : SnippetEditFormProps ) {
         options={{ minimap : {enabled: false} }}
         onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+       <button type="submit" className="p-2 border rounded">
+        Save
+       </button>
+      </form>
     </div>
   )
 };
