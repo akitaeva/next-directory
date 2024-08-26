213da649b8a4bfe935288b0fcebb06e81f0c22b5
Alina Kitaeva <akitaeva@gmail.com>
2024-08-26 15:27:18 -0400

    adds delete action to the snippet form

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { db } from '@/db';
+import { deleteSnippet } from '@/actions';
 
 interface SnippetShowPageProps {
   params: Promise<{
@@ -21,6 +22,8 @@ export default async function ShowSnippetPage(props: SnippetShowPageProps) {
     return notFound();
   }
 
+  const deleteSnippetAction = deleteSnippet.bind(null, snippet.id);
+
   return <div>
       <div className="flex m-4 justify-between items-center">
         <h1 className="text-xl font-bold">
@@ -32,7 +35,9 @@ export default async function ShowSnippetPage(props: SnippetShowPageProps) {
             className="p-2 border rounded"
           >Edit
           </Link>
-          <button className="p-2 border rounded">Delete</button>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-300">
