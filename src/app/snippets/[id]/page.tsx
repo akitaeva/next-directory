57023eae0e57a7d4d8bb89e1e0adc22b8f41271e
Alina Kitaeva <akitaeva@gmail.com>
2024-08-28 18:02:29 -0400

    adds static params to view snippet route

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -45,3 +45,14 @@ export default async function ShowSnippetPage(props: SnippetShowPageProps) {
       </pre>
     </div>
 };
+
+
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany();
+
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString,
+    }
+  });
+};
